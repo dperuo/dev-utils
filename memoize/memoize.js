e64f6ee78888df4c98f882c49961ec83a97633a8
dperuo <derek@derekperuo.net>
2017-01-31 10:17:15 -0600

    Update memoize()

--- memoize/memoize.js
+++ memoize/memoize.js
@@ -1,33 +1,31 @@
-// memoize()
-// Copywrite (c) 2017 Derek Peruo
-// Licensed under the MIT License
-// http://github.com/dperuo
-//
-// Returns a memoized instance of any function.
-//
-// @param {Funciton} callback - The funciton to memoize.
-// @return {Function} - The memoized function
-//
-// @example:
-//   var sum = memoize((...values) => values.reduce((total, value) => total + value);
-//   sum(5, 6, 7);
-//   // => 18
-"use strict";
-var memoize = function (callback) {
-    var cache = new Map();
-    return function () {
-        var inputs = [];
-        for (var _i = 0; _i < arguments.length; _i++) {
-            inputs[_i] = arguments[_i];
-        }
-        var key = JSON.stringify(inputs);
+/**
+ * memoize()
+ *
+ * Copywrite (c) 2017 Derek Peruo
+ * Licensed under the MIT License
+ * http://github.com/dperuo
+ *
+ * Returns a memoized instance of any function.
+ *
+ * @param {Funciton} callback - The funciton to memoize.
+ * @return {Function} - The memoized function
+ *
+ * @example:
+ *   var sum = memoize((...values) => values.reduce((total, value) => total + value);
+ *   sum(5, 6, 7);
+ *   // => 18
+ */
+const memoize = (callback) => {
+    const cache = new Map();
+    return (...inputs) => {
+        const key = JSON.stringify(inputs);
         if (cache.has(key)) {
             return cache.get(key);
         }
         else {
-            cache.set(key, callback.apply(void 0, inputs));
+            cache.set(key, callback(...inputs));
             return cache.get(key);
         }
     };
 };
-exports.memoize = memoize;
+export { memoize };
--- memoize/memoize.ts
+++ memoize/memoize.ts
@@ -1,17 +1,20 @@
-// memoize()
-// Copywrite (c) 2017 Derek Peruo
-// Licensed under the MIT License
-// http://github.com/dperuo
-//
-// Returns a memoized instance of any function.
-//
-// @param {Funciton} callback - The funciton to memoize.
-// @return {Function} - The memoized function
-//
-// @example:
-//   var sum = memoize((...values) => values.reduce((total, value) => total + value);
-//   sum(5, 6, 7);
-//   // => 18
+/**
+ * memoize()
+ *
+ * Copywrite (c) 2017 Derek Peruo
+ * Licensed under the MIT License
+ * http://github.com/dperuo
+ *
+ * Returns a memoized instance of any function.
+ *
+ * @param {Funciton} callback - The funciton to memoize.
+ * @return {Function} - The memoized function
+ *
+ * @example:
+ *   var sum = memoize((...values) => values.reduce((total, value) => total + value);
+ *   sum(5, 6, 7);
+ *   // => 18
+ */
 
 const memoize = (callback: Function): Function => {
   const cache: Map<string, any> = new Map();
