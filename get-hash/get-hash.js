74520f8f874606900477686622fe9e35ab6737c3
dperuo <derek@derekperuo.net>
2017-01-31 10:17:08 -0600

    Update getHash()

--- get-hash/get-hash.js
+++ get-hash/get-hash.js
@@ -1,29 +1,21 @@
-// getHash()
-// Copywrite (c) 2016 Derek Peruo
-// Licensed under the MIT License
-// http://github.com/dperuo
-//
-// Returns a random sequence of letters and numbers of any length.
-//
-// @param {number} size - The desired length of the hash
-//
-// @return {string} - The unique hash
-//
-// @example:
-//   var hash = getHash(6);
-//   // => "ll37j8"
-(function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        define([], factory);
-    }
-    else if (typeof module === 'object' && module.exports) {
-        module.exports = factory();
-    }
-    else {
-        root.getHash = factory();
-    }
-})(this, function () {
-    return function (size) {
-        return Math.floor(Math.random() * Math.pow(36, size)).toString(36);
-    };
-});
+/**
+ * getHash()
+ *
+ * Copywrite (c) 2017 Derek Peruo
+ * Licensed under the MIT License
+ * http://github.com/dperuo
+ *
+ * Returns a random sequence of letters and numbers of any length.
+ *
+ * @param {number} size - The desired length of the hash
+ *
+ * @return {string} - The unique hash
+ *
+ * @example:
+ *   var hash = getHash(6);
+ *   // => "ll37j8"
+ */
+const getHash = (size) => {
+    return Math.floor(Math.random() * Math.pow(36, size)).toString(36);
+};
+export { getHash };
--- get-hash/get-hash.ts
+++ get-hash/get-hash.ts
@@ -1,28 +1,23 @@
-// getHash()
-// Copywrite (c) 2016 Derek Peruo
-// Licensed under the MIT License
-// http://github.com/dperuo
-//
-// Returns a random sequence of letters and numbers of any length.
-//
-// @param {number} size - The desired length of the hash
-//
-// @return {string} - The unique hash
-//
-// @example:
-//   var hash = getHash(6);
-//   // => "ll37j8"
+/**
+ * getHash()
+ *
+ * Copywrite (c) 2017 Derek Peruo
+ * Licensed under the MIT License
+ * http://github.com/dperuo
+ *
+ * Returns a random sequence of letters and numbers of any length.
+ *
+ * @param {number} size - The desired length of the hash
+ *
+ * @return {string} - The unique hash
+ *
+ * @example:
+ *   var hash = getHash(6);
+ *   // => "ll37j8"
+ */
 
-((root, factory: Function) => {
-    if (typeof define === 'function' && define.amd) {
-        define([], factory);
-    } else if (typeof module === 'object' && module.exports) {
-        module.exports = factory();
-    } else {
-        root.getHash = factory();
-  }
-})(this, () => {
-    return (size: number): string => {
-      return Math.floor(Math.random() * Math.pow(36,size)).toString(36);
-    }
-});
+const getHash = (size: number): string => {
+  return Math.floor(Math.random() * Math.pow(36,size)).toString(36);
+}
+
+export { getHash };
