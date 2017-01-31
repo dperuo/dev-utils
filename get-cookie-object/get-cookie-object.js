9f1831e9322bbeb5cb70ba617edb50b08147c922
dperuo <derek@derekperuo.net>
2017-01-31 10:16:55 -0600

    Update getCookieObject()

--- get-cookie-object/get-cookie-object.js
+++ get-cookie-object/get-cookie-object.js
@@ -1,31 +1,23 @@
-// getCookieObject()
-// Copywrite (c) 2016 Derek Peruo
-// Licensed under the MIT License
-// http://github.com/dperuo
-//
-// returns an object of key-value cookie pairs
-//
-// @param {String} cookies - The document.cookie string
-//
-// @return {Object} - The cookie object
-(function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        define([], factory);
-    }
-    else if (typeof module === 'object' && module.exports) {
-        module.exports = factory();
-    }
-    else {
-        root.getCookieObject = factory();
-    }
-})(this, function () {
-    return function (cookies) {
-        var output = {};
-        var cookiesArray = cookies.split('; ');
-        cookiesArray.forEach(function (value) {
-            var item = value.split('=');
-            output[item[0]] = item[1];
-        });
-        return output;
-    };
-});
+/**
+ * getCookieObject()
+ *
+ * Copywrite (c) 2017 Derek Peruo
+ * Licensed under the MIT License
+ * http://github.com/dperuo
+ *
+ * returns an object of key-value cookie pairs
+ *
+ * @param {String} cookies - The document.cookie string
+ *
+ * @return {Object} - The cookie object
+ */
+const getCookieObject = (cookieString) => {
+    const output = {};
+    const cookies = cookieString.split('; ');
+    cookies.forEach((cookie) => {
+        const item = cookie.split('=');
+        output[item[0]] = item[1];
+    });
+    return output;
+};
+export { getCookieObject };
--- get-cookie-object/get-cookie-object.ts
+++ get-cookie-object/get-cookie-object.ts
@@ -1,32 +1,28 @@
-// getCookieObject()
-// Copywrite (c) 2016 Derek Peruo
-// Licensed under the MIT License
-// http://github.com/dperuo
-//
-// returns an object of key-value cookie pairs
-//
-// @param {String} cookies - The document.cookie string
-//
-// @return {Object} - The cookie object
+/**
+ * getCookieObject()
+ *
+ * Copywrite (c) 2017 Derek Peruo
+ * Licensed under the MIT License
+ * http://github.com/dperuo
+ *
+ * returns an object of key-value cookie pairs
+ *
+ * @param {String} cookies - The document.cookie string
+ *
+ * @return {Object} - The cookie object
+ */
 
-((root, factory) => {
-    if (typeof define === 'function' && define.amd) {
-        define([], factory);
-    } else if (typeof module === 'object' && module.exports) {
-        module.exports = factory();
-    } else {
-        root.getCookieObject = factory();
-  }
-})(this, () => {
-    return (cookies: string): Object => {
-      var output: Object = {};
-      var cookiesArray: string[] = cookies.split('; ');
+const getCookieObject = (cookieString: string): Object => {
+  const output: Object = {};
 
-      cookiesArray.forEach((value: string) => {
-        var item: string[] = value.split('=');
-        output[item[0]] = item[1];
-      });
+  const cookies = cookieString.split('; ');
 
-      return output;
-    }
-});
+  cookies.forEach((cookie: string) => {
+    const item: string[] = cookie.split('=');
+    output[item[0]] = item[1];
+  })
+
+  return output;
+}
+
+export { getCookieObject };
