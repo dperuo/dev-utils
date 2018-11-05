9e2f1091f7ccdeb1f0263cf2d210b29daf6417da
dperuo <derek@derekperuo.net>
2018-11-05 10:19:19 -0600

    Update getHash() to getRandomHash()

---
+++ get-random-hash/get-random-hash.js
@@ -0,0 +1,28 @@
+/**
+ * getRandomHash()
+ *
+ * Copywrite (c) 2017 Derek Peruo
+ * Licensed under the MIT License
+ * http://github.com/dperuo
+ *
+ * Returns a random sequence of letters and numbers of any length.
+ *
+ *
+ * @param {number} size - The desired length of the hash. Default length is 36.
+ *
+ * @return {string} - The unique hash
+ *
+ * @example:
+ *   var hash = getHash(6);
+ *   // => "ll37j8"
+ */
+const getRandomHash = (size = 12) => {
+    const hash = [];
+    for (let span = size; span--;) {
+        const char = Math.floor(Math.random() * Math.pow(36, 1)).toString(36);
+        hash.push(char);
+    }
+    const output = hash.join('');
+    return output;
+};
+export { getRandomHash };
